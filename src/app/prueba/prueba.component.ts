beb29ce7991419c787c1108b16d7019729f9c3cf
Mario <marioarjona99@gmail.com>
2024-05-08 13:12:16 +0200

    second commit

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-prueba',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FormComponent],
   templateUrl: './prueba.component.html',
   styleUrl: './prueba.component.css'
 })
